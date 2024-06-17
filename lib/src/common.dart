0b9c7f6d3a5050bde4c4f13efb49f0fc1572b9db
urusai88 <archon.saratov@gmail.com>
2024-06-17 22:28:25 +0400

    cp log

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -16,19 +16,20 @@ bool exists(String path) => FileSystemEntity(path).existsSync();
 }
 
 void mv(String src, String dst, {String? cwd}) {
-  (src, dst) => _cwd(src, dst, cwd);
+  (src, dst) = _cwd(src, dst, cwd);
   FileSystemEntity(src).renameSync(dst);
+  print('mv $src -> $dst');
 }
 
 void cp(String src, String dst, {String? cwd}) {
-  (src, dst) => _cwd(src, dst, cwd);
-
+  (src, dst) = _cwd(src, dst, cwd);
   switch (FileSystemEntity(src)) {
     case FileSystemEntity(file: final file?):
       File(dst).writeAsBytesSync(file.readAsBytesSync());
     case FileSystemEntity(directory: final directory?):
       directory.create();
   }
+  print('cp $src -> $dst');
 }
 
 void cpIfNotExists(String src, String dst, {String? cwd}) {
@@ -39,32 +40,3 @@ void cpIfNotExists(String src, String dst, {String? cwd}) {
 
 Iterable<FileSystemEntity> scan(String path) =>
     Directory(path).listSync().map((e) => FileSystemEntity(e.path));
-
-Map<String, String> buildScripts(String path) {
-  final result = <String, String>{};
-  for (final script in scan(path).where((e) =>
-      p.basename(e.path).endsWith('.dart') &&
-      !p.basename(e.path).startsWith('_'))) {
-    final scriptPath = p.normalize(p.absolute(script.path));
-    final genName = p.setExtension(scriptPath, '.exe');
-    final newName = p.setExtension(scriptPath, '.script');
-    final command = [
-      'dart',
-      'compile',
-      'exe',
-      scriptPath,
-      '-p',
-      '.dart_tool/package_config.json',
-    ];
-    final processResult = spawn(command);
-    if (processResult.stderr case final String s when s.isNotEmpty) {
-      print('===\n$command\n===');
-      print(s);
-      break;
-    }
-
-    mv(genName, newName);
-    result[genName] = newName;
-  }
-  return result;
-}
